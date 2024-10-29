d30ea58997ad2efddf4df2780c7022f82b0972df
burakdemirhan <burkidad@outlook.com>
2024-10-29 18:07:48 +0300

    first commit

---
+++ login-project/src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import Login from "./assets/components/login";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <Login></Login>;
+}
+
+export default App;
